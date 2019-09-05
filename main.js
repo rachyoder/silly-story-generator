6cec5966c8cd39c812ab0f9d818c508e552b843e
Rachael Yoder <wayoder4@gmail.com>
2019-09-05 16:44:59 -0400

    Added Comments to main.js

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
+//Establish Variables for Page Elements
 let customName = document.getElementById('customname');
 let randomize = document.querySelector('.randomize');
 let story = document.querySelector('.story');
 
+//Function to Randomize the arrays for dynamic generation
 function randomValueFromArray(array) {
     return array[Math.floor(Math.random()*array.length)];
     
 }
+
+//Story Elements
 let storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
 let insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas'];
 let insertY = ['the soup kitchen', 'Disneyland', 'the White House'];
@@ -13,25 +17,30 @@ let insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk'
 
 randomize.addEventListener('click', result);
 
+//Story Generation
 function result() {
-
     let newStory = storyText;
+    //Select random entries from Arrays
     let xItem = randomValueFromArray(insertX);
     let yItem = randomValueFromArray(insertY);
     let zItem = randomValueFromArray(insertZ);
+     
+    //Place random entries into story
+    newStory = newStory.replace(':insertX:', xItem);
     newStory = newStory.replace(':insertX:', xItem);
     newStory = newStory.replace(':insertY:', yItem);
     newStory = newStory.replace(':insertZ:', zItem);
-    newStory = newStory.replace(':insertX:', xItem);
-
+    
+    //Custom Name Setup
     if(customName.value !== '') {
         let name = customName.value;
         newStory = newStory.replace('Bob', name);
     }
-
+    
+    //Change Region Specific Story Elements
     if(document.getElementById("uk").checked) {
-        let weight = Math.round(300 / 14) + ' stone';
-        let temperature = Math.round(94) + ' centigrade';
+        let weight = Math.round(300 * .07142857) + ' stone';
+        let temperature = Math.round((94-32) * 5 / 9) + ' centigrade';
         newStory = newStory.replace('94 fahrenheit', temperature);
         newStory = newStory.replace('300 pounds', weight);
     }
